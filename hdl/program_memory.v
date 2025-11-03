997e8be3533b36f050e82cce0b7512bd18212300
Salvador Pérez del Pino <alu0101231426@ull.edu.es>
2025-11-03 22:27:30 +0000

    Fix memory paths

--- hdl/program_memory.v
+++ hdl/program_memory.v
@@ -8,7 +8,7 @@ module program_memory (
 
 	initial
 	begin
-		$readmemb("C:/Users/Usuario/Documents/clase/inf/TFG/FPGA/DE10/CPU/Multicycle/mem/program.mem", mem);
+		$readmemb("C:/Users/Usuario/Documents/clase/inf/TFG/FPGA/DE10/multicycle/mem/program.mem", mem);
 	end
 
 	assign inst = mem[addr];
